62bc85147989f22d3fc9e673c28a450352e34d17
LudmilaASouza <ludmilasouz.a@hotmail.com>
2023-08-18 18:15:55 -0300

    Construção do cabeçalho e layout HTML baseado em fragmentos.

--- goserviceapp/src/main/java/com/soulcode/goserviceapp/controller/HomeController.java
+++ goserviceapp/src/main/java/com/soulcode/goserviceapp/controller/HomeController.java
@@ -4,17 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
-@RequestMapping(value = "/")
+@Controller //Controle das rota
+@RequestMapping(value = {"/", "/home"}) //Endereçamento das rotas
 public class HomeController {
 
     @GetMapping
     public String home(){
         return "home";
     }
-
-
-
-
-
 }
+
+
