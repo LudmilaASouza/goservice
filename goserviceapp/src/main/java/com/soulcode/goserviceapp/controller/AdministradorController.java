51aa1c07b5a5993d21233440e2487fec24dd6346
LudmilaASouza <ludmilasouz.a@hotmail.com>
2023-08-25 18:10:29 -0300

    Excluir e Editar Serviço na area administrativa.

--- goserviceapp/src/main/java/com/soulcode/goserviceapp/controller/AdministradorController.java
+++ goserviceapp/src/main/java/com/soulcode/goserviceapp/controller/AdministradorController.java
@@ -1,13 +1,13 @@
 package com.soulcode.goserviceapp.controller;
 
+import com.soulcode.goserviceapp.domain.Servico;
 import com.soulcode.goserviceapp.domain.Usuario;
+import com.soulcode.goserviceapp.service.ServicoService;
 import com.soulcode.goserviceapp.service.UsuarioService;
+import com.soulcode.goserviceapp.service.exceptions.UsuarioNaoEncontradoException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -20,10 +20,49 @@ public class AdministradorController {
     @Autowired
     private UsuarioService usuarioService;
 
+    @Autowired
+    private ServicoService servicoService;
+
     @GetMapping(value = "/servicos")
-    public String servicos(){
-        return "servicosAdmin";
+    public ModelAndView servicos(){
+        ModelAndView mv = new ModelAndView("servicosAdmin");
+        try {
+            List<Servico> servicos = servicoService.findAll();
+            mv.addObject("servicos", servicos);
+        } catch (Exception ex){
+            mv.addObject("errorMessage", "Erro ao buscar dados de serviços.");
+        }
+        return mv;
     }
+
+    @PostMapping(value= "/servicos")
+    public String createService(Servico servico, RedirectAttributes attributes){
+        try{
+            servicoService.createServico(servico);
+            attributes.addFlashAttribute("successMessage", "Novo serviço adicionado com sucesso!");
+        } catch (Exception ex){
+            attributes.addFlashAttribute("errorMessage", "Erro ao adicionar novo serviço.");
+        }
+        return "redirect:/admin/servicos";
+    }
+
+    @PostMapping("/servicos/remover")
+    public String removeService(@RequestParam(name = "servicoId") Long id, RedirectAttributes attributes) {
+        try {
+            servicoService.removeServicoById(id);
+            attributes.addFlashAttribute("successMessage", "Serviço removido.");
+        } catch (Exception e) {
+            attributes.addFlashAttribute("errorMessage", "Erro ao excluir serviço.");
+        }
+        return "redirect:/admin/servicos";
+    }
+
+
+    @GetMapping(value= "/servicos/editar/{id}")
+    public String editService(@PathVariable Long id){
+        return "editarServico";
+    }
+
 
     @GetMapping(value = "/usuarios")
     public ModelAndView usuarios(){
@@ -36,7 +75,6 @@ public class AdministradorController {
         }
         return mv ;
     }
-
 
     @PostMapping(value = "/usuarios")
     public String createUser(Usuario usuario, RedirectAttributes attributes){
@@ -53,6 +91,8 @@ public class AdministradorController {
     public String disableUser(@RequestParam(name="usuarioId") Long id, RedirectAttributes attributes){
         try{
             usuarioService.disableUser(id);
+        } catch (UsuarioNaoEncontradoException ex){
+            attributes.addFlashAttribute("errorMessage", ex.getMessage());
         } catch (Exception ex){
             attributes.addFlashAttribute("errorMessage", "Erro ao desabilitar usuário.");
         }
@@ -63,6 +103,8 @@ public class AdministradorController {
     public String enableUser(@RequestParam(name="usuarioId") Long id, RedirectAttributes attributes){
         try{
             usuarioService.enableUser(id);
+        } catch (UsuarioNaoEncontradoException ex) {
+            attributes.addFlashAttribute("errorMessage", ex.getMessage());
         } catch (Exception ex){
             attributes.addFlashAttribute("errorMessage", "Erro ao habilitar usuário.");
         }
